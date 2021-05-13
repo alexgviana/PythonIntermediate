150d87af5adffc704433c6f27a76f4565a20142b
Alex Viana <alexgviana7@gmail.com>
2021-05-12 20:38:45 -0600

    venv and super list & dict, printed list on super_dict

---
+++ lists_and_dicts.py
@@ -0,0 +1,23 @@
+def run():
+    my_list=[1,"Hello", True, 4.5]
+    my_dict= {"firstname": "Alex", "lastname":"Viana"}
+
+    super_list = [
+        {"firstname": "Alex", "lastname":"Viana"},
+        {"firstname": "Adri", "lastname":"Renderos"},
+        {"firstname": "Pepe", "lastname":"Rodelo"},
+        {"firstname": "Arely", "lastname":"Viana"},
+        {"firstname": "Jose", "lastname":"Torres"}
+    ]
+
+    super_dict = {
+        "natural_nums":[1,2,3,4,5],
+        "integer_nums":[-1,-2,0,1,2],
+        "floating_nums":[1.1,4.5,6.43]
+    }
+
+    for key, value in super_dict.items():
+        print(key, "-" ,value)
+
+if __name__ == '__main__':
+    run()
